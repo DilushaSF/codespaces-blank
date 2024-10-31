0b971f6bd708228c876d4046c2585527c14f5094
Dilusha Sindutharu <sindutharu821@gmail.com>
2024-10-31 23:39:44 +0000

    Added : home page structure & Member html

--- index.js
+++ index.js
@@ -7,4 +7,31 @@ $(document).ready(function () {
       console.log("DAta", data);
     }
   );
+
+  $("#registerButton").click(function () {
+    // Place your API call here
+    $.get(
+      "http://localhost:3000/member-registration/get/all",
+      function (data, status) {
+        console.log("Data", data);
+      }
+    );
+  });
+
+  // $.get("http://localhost:3000/member-registration/get/all", function (data) {
+  //   // Assuming data is an array of member objects
+  //   data.forEach(function (member) {
+  //     $("#membersTableBody").append(
+  //       `<tr> 
+  //         <td>${member.firstName}</td> 
+  //         <td>${member.middleName}</td> 
+  //         <td>${member.dob}</td> 
+  //         <td>${member.nameOfMother}</td> 
+  //         <td>${member.nameOfFather}</td> 
+  //         <td>${member.address}</td> 
+  //         <td>${member.birthPlace}</td> 
+  //       </tr>`
+  //     );
+  //   });
+  // });
 });
