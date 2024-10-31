e737d55439868ea61e275a70e6dbb044881da085
Dilusha Sindutharu <sindutharu821@gmail.com>
2024-10-31 17:50:30 +0000

    project-init

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-$(document).ready(function(){
-    console.log("jquery loaded");
-  });
+$(document).ready(function () {
+  console.log("jquery loaded");
+  $.get(
+    "http://localhost:3000/member-registration/get/all",
+    function (data, status) {
+      // alert("Data: " + data + "\nStatus: " + status);
+      console.log("DAta", data);
+    }
+  );
+});
