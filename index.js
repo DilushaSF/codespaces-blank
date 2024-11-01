c6548f3138340ba1fb3332fc1f5c974763f244b3
Dilusha Sindutharu <sindutharu821@gmail.com>
2024-11-01 15:25:11 +0000

    Data posting and Jquery error was fixed.

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 $(document).ready(function () {
   console.log("jquery loaded");
+
   $.get(
     "http://localhost:3000/member-registration/get/all",
     function (data, status) {
-      // alert("Data: " + data + "\nStatus: " + status);
       console.log("DAta", data);
     }
   );
 
   $("#registerButton").click(function () {
-    // Place your API call here
     $.get(
       "http://localhost:3000/member-registration/get/all",
       function (data, status) {
@@ -18,48 +17,66 @@ $(document).ready(function () {
     );
   });
 
-  // $.get("http://localhost:3000/member-registration/get/all", function (data) {
-  //   // Assuming data is an array of member objects
-  //   data.forEach(function (member) {
-  //     $("#membersTableBody").append(
-  //       `<tr> 
-  //         <td>${member.firstName}</td> 
-  //         <td>${member.middleName}</td> 
-  //         <td>${member.dob}</td> 
-  //         <td>${member.nameOfMother}</td> 
-  //         <td>${member.nameOfFather}</td> 
-  //         <td>${member.address}</td> 
-  //         <td>${member.birthPlace}</td> 
-  //       </tr>`
-  //     );
-  //   });
-  // });
+  $("#addNewRegistrationButton").click(function () {
+    window.location.href = "addmembers.html";
+  });
 
+  $.get("http://localhost:3000/member-registration/get/all", function (data) {
+    data.forEach(function (member) {
+      $("#membersTableBody").append(
+        `<tr>
+          <td>${member.firstName}</td>
+          <td>${member.middleName}</td>
+          <td>${member.dob}</td>
+          <td>${member.nameOfMother}</td>
+          <td>${member.nameOfFather}</td>
+          <td>${member.address}</td>
+          <td>${member.birthPlace}</td>
+        </tr>`
+      );
+    });
+  });
 
+  $("#addMemberForm").submit(function (event) {
+    event.preventDefault();
+    let formData = {
+      firstName: $("#firstName").val(),
+      middleName: $("#middleName").val(),
+      sirName: $("#sirName").val(),
+      dob: $("#dob").val(),
+      nameOfMother: $("#nameOfMother").val(),
+      nameOfFather: $("#nameOfFather").val(),
+      address: $("#address").val(),
+      birthPlace: $("#birthPlace").val(),
+      contactNo: $("#contactNo").val(),
+      marriedDate: $("#marriedDate").val(),
+      marriedChurch: $("#marriedChurch").val(),
+    };
 
-  // $('#addMemberForm').submit(function(event) { event.preventDefault(); // Prevent the default form submission 
-  //   // Serialize form data 
-  //   let formData = { firstName: $('#firstName').val(), 
-  //     middleName: $('#middleName').val(), 
-  //     sirName: $('#sirName').val(), 
-  //     dob: $('#dob').val(), 
-  //     nameOfMother: $('#nameOfMother').val(), 
-  //     nameOfFather: $('#nameOfFather').val(), 
-  //     address: $('#address').val(), 
-  //     birthPlace: $('#birthPlace').val(), 
-  //     contactNo: $('#contactNo').val(), 
-  //     marriedDate: $('#marriedDate').val(), 
-  //     marriedChurch: $('#marriedChurch').val() 
-    
-  //   }; 
-  //     // Send the POST request 
-  //     $.post('http://localhost:3000/member-registration/create', formData, function(response) { 
-  //       // Handle the response from the server 
-  //       alert('Member added successfully!'); // Clear the form after successful submission 
-  //       $('#addMemberForm')[0].reset(); 
-  //     }).fail(function(error) { 
-  //       // Handle any errors 
-  //       alert('Error adding member: ' + error.responseText); 
-  //     }); 
-  //   });
+    $.ajax({
+      type: "POST",
+      url: "http://localhost:3000/member-registration/create",
+      data: JSON.stringify(formData),
+      contentType: "application/json",
+      success: function (response) {
+        console.log(response);
+        // Handle the response from the server
+        alert("Member added successfully!");
+        // Clear the form after successful submission
+        // $("#addMemberForm")[0].reset();
+      },
+
+      error: function (error) {
+        alert("Error adding member: " + error.responseText);
+      },
+    });
+  });
+
+  $("#addNewRegistrationButton").click(function () {
+    window.location.href = "addmembers.html";
+  });
+
+  $("#registerButton").click(function () {
+    window.location.href = "members.html";
+  });
 });
